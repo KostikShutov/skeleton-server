630f7b1356b22a4dd1ef2bc8aa63280ac475f5a8
Constantine <tift@MacBook-Air-Konstantin.local>
2023-03-27 22:52:15 +1000

    Fix bugs

--- commands/BackwardCommand.py
+++ commands/BackwardCommand.py
@@ -11,7 +11,7 @@ class BackwardCommand(CommandInterface):
     def execute(self, commandId: uuid.UUID, payload: dict) -> None:
         speed: int = int(payload['speed'])
         distance: float = float(payload['distance']) if 'distance' in payload else None
-        duration: float = (payload['duration']) if 'duration' in payload else None
+        duration: float = float(payload['duration']) if 'duration' in payload else None
 
         if distance is not None and duration is None:
             duration: float = float(distance / speed)
--- utils/Redis.py
+++ utils/Redis.py
@@ -2,4 +2,5 @@ import redis
 from utils.Config import config
 
 host: str = config['REDIS_HOST']
-redis = redis.Redis(host=host)
+port: int = config['REDIS_PORT']
+redis = redis.Redis(host=host, port=port)
